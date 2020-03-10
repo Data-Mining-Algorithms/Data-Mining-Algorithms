1ea70c0a4a56b51651485a206b8d6083eaf31ca1
Aleksandra Petkova <a.petkova1617@gmail.com>
2020-03-10 16:01:03 +0000

    Initial Commit
    Added Main File & Data Repository.

---
+++ Main.py
@@ -0,0 +1,22 @@
+# TODO:
+#######################################################################################################################
+#                                             Data Mining Algorithms                                                  #
+#                                                                                                                     #
+#           This project was developed for the Data Mining module at Teesside University with the aim to              #
+#           demonstrate and evaluate the use of popular computational techniques for data mining. The                 #
+#           project contains implementations of popular Data Mining techniques such as Dimension Reduction,           #
+#           Association Rules Mining, Collaborative Filtering and a variety of data sets to test on.                  #
+#                                                                                                                     #
+#                                                                                                                     #
+#   Data Repository Contains:                                                                                         #
+#       >                                                                                                             #
+#       >                                                                                                             #
+#                                                                                                                     #
+#                                                  Developed by                                                       #
+#                               Aleksandra Petkova, Victor Essien, Nour Al Mubarak                                    #
+#                                                                                                                     #
+#######################################################################################################################
+
+
+def main():
+    pass

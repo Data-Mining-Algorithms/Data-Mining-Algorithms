cb94132cac4e29b9b2936a898d010fcc08e229cc
Aleksandra Petkova <a.petkova1617@gmail.com>
2020-03-20 10:29:08 +0000

    Added a dictionary of algorithms

--- Main.py
+++ Main.py
@@ -18,5 +18,35 @@
 #######################################################################################################################
 
 
+# Runs association rules mining core
+def run_ARMC():
+    print("TODO: ARMC CODE HERE") # This print never runs, the dictionary ignores them. TODO: Find out why.
+    a = 10
+    a += 1
+    return a
+
+
+# Runs association rules mining with library
+def run_ARML():
+    print("TODO: ARML CODE HERE")  # This print never runs, the dictionary ignores them. TODO: Find out why.
+
+
+def run_CFL():
+    print("TODO: CFL CODE HERE")  # This print never runs, the dictionary ignores them. TODO: Find out why.
+
+
 def main():
-    pass
+
+    alg_run_dict = {'1': run_ARMC(), '2': run_ARML(), '3': run_CFL()}
+
+    print("Please select the algorithm you wish to explore! (Example: 1) \n"
+          "1) Association Rules Mining (Core Python)"
+          "2) Association Rules Mining (Library)"
+          "3) Collaborative filtering (Library)")
+
+    alg_option = input("Enter option number here: ")
+
+    print(alg_run_dict[alg_option])
+
+
+main()

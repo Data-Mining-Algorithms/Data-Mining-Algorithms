0f857a7461db35a49f20450f468773f3759e5d85
Aleksandra Petkova <a.petkova1617@gmail.com>
2020-04-20 11:38:36 +0100

    Fixed bug with loading data.

--- Apriori Lib - Groceries.py
+++ Apriori Lib - Groceries.py
@@ -3,20 +3,25 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from apyori import apriori
 import csv
-with open('groceries.csv')as f:
-  data = csv.reader(next(f))
-  for row in data:
+with open('Data Repository\\groceries.csv')as f:
+    reader_object = csv.reader(f)   # Returns a csv reader object which will have information about the file and data
+    data = list(reader_object)   # This is a more secure way to extract the data from the reader object
+
+    for row in data:
         print(row)
+
 # data = pd.read_csv('groceries.csv')
 # print(data)
 
-records = []
-for i in range(0, 7501):
-    records.append([str(data.values[i, j]) for j in range(0, 20)])
+# NON OF THIS IS NEEDED BECAUSE YOU DO NOT USE PANDAS!
+# records = []
+# for i in range(0, 7501):
+    # "data" is a python list, there is no such attribute in the list as "values"
+    # records.append([str(data.values[i, j]) for j in range(0, 20)])  # this --> data.values is the source of your bug.
 
-association_rules = apriori(records, min_support=0.5, min_confidence=0.2, min_lift=3, min_length=3)
-association_results = list(association_rules)
-# print(len(association_rules))
+association_rules = apriori(data, min_support=0.02, min_confidence=0.02, min_lift=3, min_length=3)  # Must be some issue here!
+association_results = list(association_rules)                                                       # because this is an empty list..
+#print(len(association_rules))
 # print(association_rules[1])
 print(association_rules)
 
